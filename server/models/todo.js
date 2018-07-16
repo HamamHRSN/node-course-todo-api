61163dd9fb0f1011e2aaa192d94b12e3bbd895a9
Hamam Hamou <hamamhamou.hrsa@gmail.com>
2018-07-16 22:58:41 +0200

    Setup app to heroku

--- server/models/todo.js
+++ server/models/todo.js
@@ -19,3 +19,70 @@ var Todo = mongoose.model('Todo', {
 });
 
 module.exports = {Todo};
+
+// var Todo = mongoose.model('Todo', {
+//     text: {
+//         type: String
+//     },
+//     completed:{
+//         type: Boolean
+//     },
+//     completedAt: {
+//         type: Number
+//     }
+// });
+
+// var Todo = mongoose.model('Todo', {
+//     text: {
+//         type: String,
+//         required: true,
+//         minlength: 1,
+//         trim: true
+
+//     },
+//     completed:{
+//         type: Boolean,
+//         default: false
+//     },
+//     completedAt: {
+//         type: Number,
+//         default:null
+//     }
+// });
+
+
+// var newTodo = new Todo({
+//     text:'Cook dinner'
+// });
+
+// newTodo.save().then((doc) => {
+//     console.log('Saved Todo', doc);
+    
+// }, (e) => {
+//     console.log('Unable to save Todo',e);
+    
+// });
+
+// var otherTodo = new Todo({
+//     text: 'Feed the cat',
+//     completed:true,
+//     completedAt:123
+// });
+// otherTodo.save().then((doc) => {
+//     console.log(JSON.stringify(doc, undefined, 2));
+    
+// }, (e) => {
+//     console.log('Unable to Add otherTodo', e);
+    
+// })
+
+// var otherTodo2 = new Todo({
+//     text: 'Edit the video',
+// });
+// otherTodo2.save().then((doc) => {
+//     console.log(JSON.stringify(doc, undefined, 2));
+    
+// }, (e) => {
+//     console.log('Unable to Add otherTodo', e);
+    
+// });
--- server/models/user.js
+++ server/models/user.js
@@ -15,3 +15,41 @@ var User = mongoose.model('users',{
 });
 
 module.exports = {User};
+
+
+
+
+
+// var User = mongoose.model('users',{
+//     name:{
+//          type: String,
+//          trim: true,
+//          minlength: 1
+
+//     },
+//     email:{
+//         type:String,
+//         minlength: 5,
+//         required: true,
+//         trim: true,
+        
+//     },
+//     password: {
+//         type: String,
+//         minlength:5
+//     } 
+    
+// });
+
+// var user = new User({
+//   name: 'Rouaa Aljebbeh         ',
+//   email: 'rouaa@gmail.             com',
+//   password:'123abc'   
+// });
+// user.save().then((doc) => {
+//     console.log('Saved User', doc);
+    
+// }, (e) => {
+//     console.log('Unable to save User');
+    
+// });
